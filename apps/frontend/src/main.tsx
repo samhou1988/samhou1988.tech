dd1c873cd2a08cd2d260e27e7008ed51404245ed
samhou1988 <samhou1988@gmail.com>
2023-07-21 22:30:07 +0800

    feat: frontend configure eslint and prettier

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 function App() {
-  return <div>Hello World</div>
+  return <div>Hello World</div>;
 }
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
